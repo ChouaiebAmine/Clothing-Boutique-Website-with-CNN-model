00cfaa4c6f9dafa90368d4aa0d8ad9c2a3315a95
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-04-21 17:38:00 +0100

    passing an image to

--- app.py
+++ app.py
@@ -4,14 +4,6 @@ from PIL import Image
 import base64
 import io
 
-
-im = picturesRandomizer.get_clothing_piece()
-im = Image.fromarray(im, "RGB")
-data = io.BytesIO()
-im.save(data, "JPEG")
-encim = base64.b64encode(data.getvalue())
-
-
 # Create a Flask application instance
 app = Flask(__name__, template_folder="template")
 
@@ -19,13 +11,18 @@ app = Flask(__name__, template_folder="template")
 # Define a route for the root URL
 @app.route("/")
 def index():
-    return render_template("index.html")
+    im = picturesRandomizer.get_clothing_piece()
+    im = Image.fromarray(im, "RGB")
+    data = io.BytesIO()
+    im.save(data, "JPEG")
+    encim = base64.b64encode(data.getvalue())
+    return render_template("index.html",img_data=encim.decode('utf-8'))
 
 
-@app.route("/welcome", methods=["POST", "GET"])
+"""@app.route("/welcome", methods=["POST", "GET"])
 def greet():
     return render_template("index.html")
-
+"""
 
 # Run the Flask application
 if __name__ == "__main__":
