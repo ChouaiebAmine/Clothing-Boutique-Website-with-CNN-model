e593e268b4ee5ffbca865c3dc918daf2564a1b90
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-04-21 15:25:27 +0100

    added picturesRandomizer module

---
+++ picturesRandomizer.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import random
+import pandas as pd
+df=pd.read_csv("fashion-mnist_train.csv").values
+i = random.randint(1,60000) # select any random index from 1 to 60,000
+plt.imshow( df[i,1:].reshape((28,28)) ) # reshape and plot the image
+
+plt.imshow( df[i,1:].reshape((28,28)) , cmap = 'gray') # reshape and plot the image
