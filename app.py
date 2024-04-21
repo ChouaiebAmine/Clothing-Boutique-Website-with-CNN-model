1c1e3f5c5afe2cf7568f101b0d07f0ac75799833
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-04-21 17:37:26 +0100

    passing an image to index

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask,render_template,flash,request
+from flask import Flask, render_template, flash, request
 import picturesRandomizer
 from PIL import Image
 import base64
 import io
 
-im=Image.open("test.jpg")
-data=io.BytesIO()
-im.save(data,"JPEG")
-encim=base64.b64encode(data.getvalue())
+
+im = picturesRandomizer.get_clothing_piece()
+im = Image.fromarray(im, "RGB")
+data = io.BytesIO()
+im.save(data, "JPEG")
+encim = base64.b64encode(data.getvalue())
 
 
 # Create a Flask application instance
-app = Flask(__name__,template_folder='template')
+app = Flask(__name__, template_folder="template")
+
+
 # Define a route for the root URL
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
-"""@app.route("/welcome",methods=['POST','GET'])
+
+@app.route("/welcome", methods=["POST", "GET"])
 def greet():
-    return render_template("index.html")"""
+    return render_template("index.html")
+
+
 # Run the Flask application
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
