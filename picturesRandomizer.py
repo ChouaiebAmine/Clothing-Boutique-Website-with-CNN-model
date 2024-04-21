bb2697a013ede41954a9ad66511fd77be51eb0cb
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-04-21 16:26:18 +0100

    added function that returns a random image

--- picturesRandomizer.py
+++ picturesRandomizer.py
@@ -2,9 +2,7 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 import pandas as pd
-df=pd.read_csv("fashion-mnist_train.csv").values
-i = random.randint(1,60000) # select any random index from 1 to 60,000
-plt.imshow( df[i,1:].reshape((28,28)) ) # reshape and plot the image
-
-plt.imshow( df[i,1:].reshape((28,28)) , cmap = 'gray') # reshape and plot the image
-plt.show()
+def get_clothing_piece():
+    df=pd.read_csv("fashion-mnist_train.csv").values
+    i = random.randint(1,60000) # select any random index from 1 to 60,000
+    return df[i,1:].reshape((28,28)) # reshape and return the image
