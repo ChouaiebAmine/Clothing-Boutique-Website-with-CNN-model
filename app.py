acddec294d98360400d8afeabc4bd49e82b30b4b
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-04-21 16:12:10 +0100

    c

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask,render_template,flash,request
+import picturesRandomizer
+from PIL import Image
+import base64
+import io
+
+im=Image.open("test.jpg")
+data=io.BytesIO()
+im.save(data,"JPEG")
+encim=base64.b64encode(data.getvalue())
+
+
 # Create a Flask application instance
 app = Flask(__name__,template_folder='template')
 # Define a route for the root URL
