97deba51a97d5860491299569ecb5be2cada3f0c
Amen-Haj-Brahim <amanallahhajbrahim2003@gmail.com>
2024-04-21 16:12:15 +0100

    c

--- picturesRandomizer.py
+++ picturesRandomizer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 import pandas as pd
 df=pd.read_csv("fashion-mnist_train.csv").values
 i = random.randint(1,60000) # select any random index from 1 to 60,000
