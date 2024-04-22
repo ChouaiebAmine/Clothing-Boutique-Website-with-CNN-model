9cd64d658ecfd775c6899903ea15e0be9cd5c400
amanallahhajbrahim2003@gmail.com <amanallahhajbrahim2003@gmail.com>
2024-04-22 17:10:29 +0100

    RANDOMIZER WORKS !!!!

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,18 +1,46 @@
 from flask import Flask, render_template, flash, request
 import picturesRandomizer
 import os
+import random
+
 # Create a Flask application instance
 app = Flask(__name__, template_folder="template")
 
 
 # make 9 random images to display at first in the grid
-imgs=["static/images/T-shirt/tshirt ("+str(i)+").jpg" for i in range(1,10)]
-imnames=[x[x.rfind("/")+1:x.rfind(".")] for x in imgs]
-print(imnames)
+classes = [
+    # "ankle boot",
+    "bag",
+    # "coat",
+    "dress",
+    # "pullover",
+    "sandal",
+    "shirt",
+    # "sneakers",
+    "t shirt",
+    "trouser",
+]
+
 # Define a route for the root URL
 @app.route("/")
 def index():
-    return render_template("index.html",im_info=zip(imgs,imnames))
+    imgs = []
+    imnames = []
+    for i in range(0, 9):
+        randclass = random.randint(0, 5)
+        randpic=random.randint(1,100)
+        imgs.append(
+            "static/images/"
+            + str(classes[randclass])
+            + "/"
+            + str(classes[randclass])
+            + " ("
+            + str(randpic)
+            + ").jpg"
+        )
+        imnames.append(str(classes[randclass]) + " (" + str(randpic) + ")")
+    return render_template("index.html", im_info=zip(imgs, imnames))
+
 
 """@app.route("/welcome", methods=["POST", "GET"])
 def greet():
