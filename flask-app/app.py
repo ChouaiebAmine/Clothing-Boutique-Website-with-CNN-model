f9001181253a88515a283848ed95e5a75b2159fa
amanallahhajbrahim2003@gmail.com <amanallahhajbrahim2003@gmail.com>
2024-04-22 15:17:51 +0100

    grid works needs randomizer

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,22 +5,14 @@ import os
 app = Flask(__name__, template_folder="template")
 
 
-
-# remove pictures if there are any
-try:
-    for i in range(1,10):
-        os.remove('static/images/'+str(i)+'.png')
-except:
-    pass
 # make 9 random images to display at first in the grid
-for i in range(1, 10):
-    picturesRandomizer.get_clothing_piece(i)
-
+imgs=["static/images/T-shirt/tshirt ("+str(i)+").jpg" for i in range(1,10)]
+imnames=[x[x.rfind("/")+1:x.rfind(".")] for x in imgs]
+print(imnames)
 # Define a route for the root URL
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html",im_info=zip(imgs,imnames))
 
 """@app.route("/welcome", methods=["POST", "GET"])
 def greet():
