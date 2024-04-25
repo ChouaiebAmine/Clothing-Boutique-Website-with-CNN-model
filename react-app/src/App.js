32976c3b28dc95046342ff808afb90e5b3989f49
amanallahhajbrahim2003@gmail.com <amanallahhajbrahim2003@gmail.com>
2024-04-25 11:16:38 +0100

    BACK END TO FRONT END WORKS NOW

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,20 +7,20 @@ import ImageUpload from './ImageUpload';*/
 import './App.css';
 import axios from 'axios'
 
-const App = () => {
+function App() {
   //const [theme, setTheme] = useState('light');
 
   /*const toggleTheme = () => {
     setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
   };*/
   const [data, setData] = useState(0)
-  const fetchAPI=async()=>{
-    const res=await axios.get("http://127.0.0.1:5000")
+  const fetchAPI = async () => {
+    const res = await axios.get("http://127.0.0.1:5000")
     console.log(res.data.test);
   };
-  useEffect(()=>{
+  useEffect(() => {
     fetchAPI();
-  },[]);
+  }, []);
   /*useEffect(() => {
     axios.get("http://127.0.0.1:5000").then(
       res => {setData(res.data.data);}
@@ -50,7 +50,7 @@ const App = () => {
     </div>*/
     <div>
 
-      {(typeof data.test )=== 'undefined' ? (
+      {(typeof data.test) === 'undefined' ? (
         <p>wait</p>
       ) : (
         data.test.map((wa, i) => (
